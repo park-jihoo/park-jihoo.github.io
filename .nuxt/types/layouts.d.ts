1d45d0425ec1c48b6b058d7f1879c7197bf3f2ad
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2023-08-26 16:43:30 +0900

    convert to nuxt

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default"
-declare module "/Users/jihoo/githubio/park-jihoo.github.io/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
--- .output/public/_nuxt/code-41222ca7.f6a88cca.js
+++
@@ -1,1 +0,0 @@
-import{m as t}from"./index-4beab083.aa6fe3da.js";import"./entry.f1d16f64.js";export{t as default};
