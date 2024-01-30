f04dbddb98c5b072480bd8c09cf687ac290a657d
Jihoo Park <67787453+park-jihoo@users.noreply.github.com>
2024-01-30 23:39:15 +0900

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,13 +18,14 @@ export function generateMetaData() {
     title: "Park Jihoo",
     description: "Park Jihoo's personal website",
     image: "https://avatars.githubusercontent.com/u/24237865?v=4",
-    url: new URL("https://park-jihoo.github.io/"),
+    url: "https://park-jihoo.github.io/",
     verification : {
       google: "kQoktB1lk0tqFRLs4-vqhVfEVWlRMX8VRQtLC_L09uw",
       other: {
         "naver-site-verification": "0581245626108d8ea24cc4d24a30ee84c0394ce6",
       }
-    }
+    },
+    metadataBase: new URL("https://park-jihoo.github.io/"),
   };
 }
 
